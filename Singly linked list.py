4326aa2c47fc7b09fd3a9969a9f92e83ddb8f0ec
Revanth-123663 <venkatarevanthpodili@gmail.com>
2025-12-19 09:42:21 +0530

    Create Singly linked list.py

---
+++ Singly linked list.py
@@ -0,0 +1,27 @@
+class SinglyNode:
+
+  def __init__(self, val, next=None):
+    self.val = val
+    self.next = next
+
+  def __str__(self):
+    return str(self.val)
+
+Head = SinglyNode(1)
+A = SinglyNode(3)
+B = SinglyNode(4)
+C = SinglyNode(7)
+
+Head.next = A
+A.next = B
+B.next = C
+
+def display(head):
+  curr = head
+  elements = []
+  while curr:
+    elements.append(str(curr.val))
+    curr = curr.next
+  print(' -> '.join(elements))
+
+display(Head)
